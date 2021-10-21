85d5f2c8211c868be872263f9ca2bdb8ecb1f593
me.ki <walk.beaute@gmail.com>
2021-10-21 06:35:00 +0000

    create shop-info view

--- resources/views/shop/index.blade.php
+++ resources/views/shop/index.blade.php
@@ -14,36 +14,88 @@
   
   <div class="container mt-3">
     <div class="row mt-3">
-        <!--メイン-->
-        <div class="information col-sm-8">
-            <h4 class="title mr-auto mt-1 border-bottom">店舗一覧</h4>
-            
-            <div class="shop-info d-flex flex-row">
-              <p class="shop">
-                <img src="/image/office.jpg" alt="shop" class="img-fluid rounded">
-              </p>
-              <table class="table">
-                  <tr class="shop-name">
-                      <th colspan="2">MI-KI MANSION 谷山店</th>
-                  </tr>
-                  <tr>
-                      <th>所在地</th>
-                      <tb>鹿児島県鹿児島市〇〇〇</tb>
-                  </tr>
-                  <tr>
-                      <th>営業時間</th>
-                      <tb>10:00－18:00</tb>
-                  </tr>
-                  <tr>
-                      <th>定休日</th>
-                      <tb>水曜日・祝日</tb>
-                  </tr>
-              </table>
+      <!--メイン-->
+      <div class="information col-sm-8">
+        <h4 class="title mr-auto mt-1">店舗一覧</h4>
+        
+        <div class="shop-info border d-flex flex-row mt-2">
+          <div class="shop-img d-flex align-items-center ml-1">
+            <img src="/image/office.jpg" alt="shop" class="img-fluid rounded">
+          </div>
+          <div class="shop-detail mt-3">
+            <h5 class="shop-name font-weight-bold my-2">ME-KI MSNSION 谷山店</h5>
+            <div class="mt-4 ml-2">
+              <p class="font-weight-bold border-bottom mr-5">所在地</p>
+              <p class="detail-text">鹿児島県鹿児島市〇〇〇</p>
             </div>
+            <div class="mt-2 ml-2">
+              <p class="font-weight-bold border-bottom mr-5">営業時間</p>
+              <p class="detail-text">10：00－18：00</p>
+            </div>
+            <div class="mt-2 ml-2">
+              <p class="font-weight-bold border-bottom mr-5">定休日</p>
+              <p class="detail-text">水曜日・祝日</p>
+            </div>
+            <div class="mt-2 ml-2">
+              <p class="font-weight-bold border-bottom mr-5">電話番号</p>
+              <p class="detail-text">000-000-0000</p>
+            </div>
+          </div>
         </div>
         
-        <!--サイドメニュー-->
-        @include('commons.sidebar')
+        <div class="shop-info border d-flex flex-row mt-4">
+          <div class="shop-img d-flex align-items-center ml-1">
+            <img src="/image/office.jpg" alt="shop" class="img-fluid rounded">
+          </div>
+          <div class="shop-detail mt-3">
+            <h5 class="shop-name font-weight-bold my-2">ME-KI MSNSION 鹿屋店</h5>
+            <div class="mt-4 ml-2">
+              <p class="font-weight-bold border-bottom mr-5">所在地</p>
+              <p class="detail-text">鹿児島県鹿児島市〇〇〇</p>
+            </div>
+            <div class="mt-2 ml-2">
+              <p class="font-weight-bold border-bottom mr-5">営業時間</p>
+              <p class="detail-text">10：00－18：00</p>
+            </div>
+            <div class="mt-2 ml-2">
+              <p class="font-weight-bold border-bottom mr-5">定休日</p>
+              <p class="detail-text">水曜日・祝日</p>
+            </div>
+            <div class="mt-2 ml-2">
+              <p class="font-weight-bold border-bottom mr-5">電話番号</p>
+              <p class="detail-text">000-000-0000</p>
+            </div>
+          </div>
+        </div>
+        
+        <div class="shop-info border d-flex flex-row mt-4">
+          <div class="shop-img d-flex align-items-center ml-1">
+            <img src="/image/office.jpg" alt="shop" class="img-fluid rounded">
+          </div>
+          <div class="shop-detail mt-3">
+            <h5 class="shop-name font-weight-bold my-2">ME-KI MSNSION 霧島店</h5>
+            <div class="mt-4 ml-2">
+              <p class="font-weight-bold border-bottom mr-5">所在地</p>
+              <p class="detail-text">鹿児島県鹿児島市〇〇〇</p>
+            </div>
+            <div class="mt-2 ml-2">
+              <p class="font-weight-bold border-bottom mr-5">営業時間</p>
+              <p class="detail-text">10：00－18：00</p>
+            </div>
+            <div class="mt-2 ml-2">
+              <p class="font-weight-bold border-bottom mr-5">定休日</p>
+              <p class="detail-text">水曜日・祝日</p>
+            </div>
+            <div class="mt-2 ml-2">
+              <p class="font-weight-bold border-bottom mr-5">電話番号</p>
+              <p class="detail-text">000-000-0000</p>
+            </div>
+          </div>
+        </div>
+        
+      </div>
+      <!--サイドメニュー-->
+      @include('commons.sidebar')
     </div>
   </div>
   
